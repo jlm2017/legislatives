1dfdbca7dc24eae7689977a99344c42ca0028c50
AlexisJC <Cuellar.AlexisJ@gmail.com>
2017-02-03 12:04:43 +0100

    Update model Lien

--- app/Lien.php
+++ app/Lien.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Lien extends Model
 {
     //
-    protected $fillable = ['idCandidat', 'nom', 'url'];
+    protected $fillable = ['idCandidat', 'label', 'url'];
 
     public $timestamps = false;
 }
